2e3c5842614870d99e2fedd572c4d2399c1d7f61
guilhermealbm <gui.miranda.13@gmail.com>
2021-05-12 10:52:33 -0300

    update query.sql

--- query.sql
+++ query.sql
@@ -19,3 +19,28 @@ SET @LitElement = ('lit-element');
 SET @Stimulus = ('stimulusjs');
 SET @Angular = ('angular', 'angular2', 'angular4', 'angular4.x', 'angularjs2'); -- doesn't include AngularJS (1.x)
 SET @Ember = ('ember.js', 'ember', 'emberjs');
+
+-- Data Layer
+-- GraphQL, Apollo Client, Vuex, XState, Redux, MobX, Relay
+
+SET @GraphQL = ('graphql');
+SET @ApolloClient = ('apollo-client');
+SET @Vuex = ('vuex');
+SET @XState = ('xstate');
+SET @Redux = ('redux');
+SET @MobX = ('mobx');
+SET @Relay = ('relay', 'relayjs'); -- Suggested synonym
+
+-- Back-end Frameworks
+-- Next.js, Express, Fastify, Nuxt, Nest, Strapi, Koa, Gatsby, Hapi, Meteor
+
+SET @Nextjs = ('next.js', 'nextjs');
+SET @Express = ('express', 'express.js', 'expressjs');
+SET @Fastify = ('fastify');
+SET @Nuxt = ('nuxt.js', 'nuxt', 'nuxtjs'); -- Suggested synonym
+SET @Nest = ('nestjs');
+SET @Strapi = ('strapi');
+SET @Koa = ('koa');
+SET @Gatsby = ('gatsby', 'gatsbyjs'); -- Suggested synonym
+SET @Hapi = ('hapijs');
+SET @Meteor = ('meteor', 'meteorjs', 'meteor.js');
