5537178043e86b0e969cec34219c9ce573158373
guilhermealbm <gui.miranda.13@gmail.com>
2021-05-12 09:56:08 -0300

    add query.sql

---
+++ query.sql
@@ -0,0 +1,21 @@
+-- JavaScript Flavors
+-- TypeScript, PureScript, Reason, Elm, ClojureScript
+
+SET @TypeScript = ('typescript', 'atscript');
+SET @PureScript = ('purescript');
+SET @Reason = ('reason', 'reasonml');
+SET @Elm = ('eml');
+SET @ClojureScript = ('clojurescript');
+
+-- Front-end Frameworks
+-- Svelte, React, Vue.js, Alpine.js, Preact, LitElement, Stimulus, Angular, Ember
+
+SET @Svelte = ('svelte');
+SET @React = ('reactjs', 'react', 'react-jsx', 'react.js');
+SET @Vuejs = ('vue.js', 'vuejs', 'vue', 'vue-js');
+SET @Alpine = ('alpine.js');
+SET @Preact = ('preact');
+SET @LitElement = ('lit-element');
+SET @Stimulus = ('stimulusjs');
+SET @Angular = ('angular', 'angular2', 'angular4', 'angular4.x', 'angularjs2'); -- doesn't include AngularJS (1.x)
+SET @Ember = ('ember.js', 'ember', 'emberjs');
