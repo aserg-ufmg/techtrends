2b1c7b72e5c826b69fb8ddcf520b9cec1b0b2021
guilhermealbm <gui.miranda.13@gmail.com>
2021-05-31 12:11:01 -0300

    sort array for show legend in decrescent order

--- scripts.js
+++ scripts.js
@@ -63,7 +63,7 @@ var mobileAndDesktop = ["electron",
 "quasar-framework",
 "ionic-framework", "ionic",
 "nwjs",
-"cordova", "apache-cordova"];
+"cordova", "apache-cordova"];  
 
 var categoriesArray = [flavors, frontFrameworks, dataLayers, backendFrameworks, testing, buildTools, mobileAndDesktop];
 var idArrays = ["flavorsSvg", "frontFrameworksSvg", "dataLayersSvg", "backendFrameworksSvg", "testingSvg", "buildToolsSvg", "mobileAndDesktopSvg"];
@@ -142,9 +142,8 @@ function drawGraph() {
             .style("alignment-baseline", "middle")
 
         // Loop through each symbol / key
-        var j = 1;
+        var lastValueArray = []
         dataNest.forEach(function(d,i) { 
-            var size = 20;
 
             if(categoriesArray[index].some(e => e == d.key)) {
 
@@ -155,25 +154,33 @@ function drawGraph() {
                         return d.color = color(i); })
                     .attr("d", questionsline(d.value));
 
-                svg.append("rect")
-                        .attr("x", 5)
-                        .attr("y", 5 + j*(size+5)) // 100 is where the first dot appears. 25 is the distance between dots
-                        .attr("width", size)
-                        .attr("height", size)
-                        .style("fill", d.color)
+                lastValueArray.push([d.key, d.value[d.value.length - 1].questions, d.color])
 
-                // Add one dot in the legend for each name.
-                svg.append("text")
-                        .attr("x", 5 + size*1.2)
-                        .attr("y", 5 + j*(size+5) + (size/2)) // 100 is where the first dot appears. 25 is the distance between dots
-                        .style("fill", d.color)
-                        .text(d.key)
-                        .attr("text-anchor", "left")
-                        .style("alignment-baseline", "middle")
-
-                j++
             }
 
+        });
+
+        lastValueArray.sort(function(a,b) {
+            return b[1]-a[1]
+        });        
+
+        lastValueArray.forEach(function(d, i) {
+            var size = 20;
+            svg.append("rect")
+                .attr("x", 5)
+                .attr("y", 5 + (i+1)*(size+5)) // 100 is where the first dot appears. 25 is the distance between dots
+                .attr("width", size)
+                .attr("height", size)
+                .style("fill", d[2])
+
+             // Add one dot in the legend for each name.
+            svg.append("text")
+                .attr("x", 5 + size*1.2)
+                .attr("y", 5 + (i+1)*(size+5) + (size/2)) // 100 is where the first dot appears. 25 is the distance between dots
+                .style("fill", d[2])
+                .text(d[0])
+                .attr("text-anchor", "left")
+                .style("alignment-baseline", "middle")
         });
 
         // Add the X Axis
